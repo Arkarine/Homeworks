da3089f0e35c998f038ef81d2a30fe32b4c89f71
Arkarine <karine.arustamyan@gmail.com>
2019-07-19 15:18:54 +0200

    The same task through Stream API

--- DateTimeUsing/src/DateFormatModifier.java
+++ DateTimeUsing/src/DateFormatModifier.java
@@ -3,12 +3,24 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DateFormatModifier {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("2019-05-18", "2015-04-25", "2009-04-22");
         System.out.println("Old list" + '\n' + list);
         System.out.println("Formatted list" + '\n' + reformatDates(list));
+
+        //The same task through Stream API
+
+        List<String>  reformattedDates = list
+                .stream()
+                .map(s -> LocalDate.parse(s))
+                .map(date -> date.format(DateTimeFormatter.ofPattern("(dd. MMM, yyyy EEEE)")))
+                .collect(Collectors.toList());
+
+        System.out.println(reformattedDates);
+
 
     }
 
