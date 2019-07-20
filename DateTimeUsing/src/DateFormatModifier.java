7fe418203618106717629667189fbcc4a4a0a46c
Arkarine <karine.arustamyan@gmail.com>
2019-07-20 19:25:23 +0200

    Add junit test and sort function

--- DateTimeUsing/src/DateFormatModifier.java
+++ DateTimeUsing/src/DateFormatModifier.java
@@ -3,6 +3,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class DateFormatModifier {
@@ -10,21 +11,42 @@ public class DateFormatModifier {
         List<String> list = Arrays.asList("2019-05-18", "2015-04-25", "2009-04-22");
         System.out.println("Old list" + '\n' + list);
         System.out.println("Formatted list" + '\n' + reformatDates(list));
+    }
 
-        //The same task through Stream API
+    //The task through Stream API
+
+    public static List<String> reformatDates(List<String> list){
+        DateTimeFormatter formatterIn = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter formatterOut = DateTimeFormatter.ofPattern("dd MMMM, yyyy EEEE", Locale.ENGLISH);
+
 
         List<String>  reformattedDates = list
                 .stream()
-                .map(s -> LocalDate.parse(s))
-                .map(date -> date.format(DateTimeFormatter.ofPattern("(dd. MMM, yyyy EEEE)")))
+                .map(s -> LocalDate.parse(s,formatterIn))
+                .map(date -> date.format(formatterOut))
                 .collect(Collectors.toList());
 
-        System.out.println(reformattedDates);
-
-
+        return reformattedDates;
     }
 
-    public static List<String> reformatDates(List<String> list){
+    public static List<String> reformatSortDates(List<String> list){
+        DateTimeFormatter formatterIn = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter formatterOut = DateTimeFormatter.ofPattern("dd MMMM, yyyy EEEE", Locale.ENGLISH);
+
+
+        List<String>  reformattedSortedDates = list
+                .stream()
+                .map(s -> LocalDate.parse(s,formatterIn))
+                .sorted()
+                .map(date -> date.format(formatterOut))
+                .collect(Collectors.toList());
+
+        return reformattedSortedDates;
+    }
+
+    //The task through loop
+
+    public static List<String> reformatDates1(List<String> list){
         List<LocalDate> dates = new ArrayList<>();
 
         for (String s : list) {
